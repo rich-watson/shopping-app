375e3481d997f2df592025137af9e83611af9f60
rich-watson <62966530+rich-watson@users.noreply.github.com>
2021-03-09 21:45:40 -0600

    more testing

--- src/test/java/edu/depaul/se433/shoppingapp/equivalenceTests.java
+++ src/test/java/edu/depaul/se433/shoppingapp/equivalenceTests.java
@@ -1,5 +1,15 @@
 package edu.depaul.se433.shoppingapp;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class equivalenceTests {
+
+
+
+
+
+
 
 }
